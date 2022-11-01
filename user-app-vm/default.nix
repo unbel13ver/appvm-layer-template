5e25f3599f5cbfce1090350eb85e5905d31364b3
Ivan Nikolaenko <ivan.nikolaenko@unikie.com>
2022-11-01 20:08:58 +0200

    Fix calling script for Chromium
    Signed-off-by: Ivan Nikolaenko <ivan.nikolaenko@unikie.com>

--- user-app-vm/default.nix
+++ user-app-vm/default.nix
@@ -6,10 +6,21 @@
 
 import ../../spectrum/vm-lib/make-vm.nix { inherit config; } {
     name = "appvm-user";
+    wayland = true;
     run = config.pkgs.callPackage (
-      { writeShellScript, chromium }:
-      writeShellScript "appvm-chromium-run" ''
-        ${chromium}/bin/chromium  --enable-features=UseOzonePlatform --ozone-platform=wayland --no-sandbox
+      { writeScript, chromium, wayland-proxy-virtwl }:
+      writeScript "appvm-chromium-run" ''
+        #!/bin/execlineb -P
+        if { modprobe virtio-gpu }
+        foreground { ln -ns /run/ext /run/opengl-driver }
+        foreground { mkdir /run/user }
+        foreground {
+          umask 077
+          mkdir /run/user/0
+        }
+        export XDG_RUNTIME_DIR /run/user/0
+        ${wayland-proxy-virtwl}/bin/wayland-proxy-virtwl --virtio-gpu
+        "${chromium}/bin/chromium  --enable-features=UseOzonePlatform --ozone-platform=wayland --no-sandbox"
       ''
     ) {};
 }
