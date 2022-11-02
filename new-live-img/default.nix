e1ba9593d64ac432a466f9ae018592ec2c7f2a92
Ivan Nikolaenko <ivan.nikolaenko@unikie.com>
2022-11-02 17:01:19 +0200

    WIP: Add uboot in a correct way
    Signed-off-by: Ivan Nikolaenko <ivan.nikolaenko@unikie.com>

--- new-live-img/default.nix
+++ new-live-img/default.nix
@@ -4,18 +4,19 @@
 
 let
   inherit (config) pkgs;
-  uboot = pkgs.ubootIMX8QM;
+  uboot = pkgs.callPackage ../bsp/imx8qm/imx-uboot.nix { inherit pkgs; };
   spectrum = import ../../spectrum/release/live { };
   kernel = spectrum.rootfs.kernel;
   appvm-user = pkgs.callPackage ../user-app-vm/default.nix { inherit config; };
   myextpart = with pkgs; runCommand "myext.ext4" {
-    nativeBuildInputs = [ e2tools e2fsprogs util-linux tar2ext4 libguestfs-with-appliance ];
+    nativeBuildInputs = [ e2tools e2fsprogs util-linux tar2ext4 libguestfs libguestfs-appliance ];
   } ''
+    export LIBGUESTFS_PATH=${libguestfs-appliance}/
     cp ${spectrum.EXT_FS} myext.ext4
     mkdir mp
-    ${libguestfs-with-appliance}/bin/guestmount -a myext.ext4 -m /dev/sda --rw ./mp
+    ${libguestfs}/bin/guestmount -a myext.ext4 -m /dev/sda --rw ./mp
     tar -C ${appvm-user} -c data | tar -C mp/svc -x
-    ${libguestfs-with-appliance}/bin/guestunmount mp
+    ${libguestfs}/bin/guestunmount mp
     mv myext.ext4 $out
   '';
 in
@@ -33,16 +34,15 @@ spectrum.overrideDerivation (oldAttrs: {
       echo '+6M,' | sfdisk --move-data $pname -N $partnum
       partnum=$((partnum-1))
     done
-    dd if=${uboot}/flash.bin of=$pname bs=1k seek=32 conv=notrunc
-    IMG=$pname
-    ESP_OFFSET=$(sfdisk --json $IMG | jq -r '
+    dd if=$uboot/flash.bin of=$pname bs=1k seek=32 conv=notrunc
+    ESP_OFFSET=$(sfdisk --json $pname | jq -r '
       # Partition type GUID identifying EFI System Partitions
       def ESP_GUID: "C12A7328-F81F-11D2-BA4B-00A0C93EC93B";
       .partitiontable |
       .sectorsize * (.partitions[] | select(.type == ESP_GUID) | .start)
     ')
     mcopy -no -i $pname@@$ESP_OFFSET ${kernel}/dtbs/freescale/imx8qm-mek-hdmi.dtb ::/
-    mcopy -no -i $pname@@$ESP_OFFSET ${config.pkgs.imx-firmware}/hdmitxfw.bin ::/
+    #mcopy -no -i $pname@@$ESP_OFFSET ${config.pkgs.imx-firmware}/hdmitxfw.bin ::/
     mv $pname $out
     runHook postInstall
   '';
