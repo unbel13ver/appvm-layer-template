74d448e735569581f9a51278e93fb452e7ee6387
Ivan Nikolaenko <ivan.nikolaenko@unikie.com>
2022-11-02 10:45:46 +0200

    Make i.MX8 kernel great again
    Signed-off-by: Ivan Nikolaenko <ivan.nikolaenko@unikie.com>

--- config.nix
+++ config.nix
@@ -3,15 +3,19 @@
     overlays = [
       (self: super:
         {
-          linux_imx8 = super.linuxKernel.kernels.linux_imx8.override {
+          linux_imx8 = super.linux_imx8.override {
             structuredExtraConfig = with self.lib.kernel; {
-              ATA_PIIX = yes;
               EFI_STUB = yes;
               EFI = yes;
               VIRTIO = yes;
               VIRTIO_PCI = yes;
               VIRTIO_BLK = yes;
+              DRM_VIRTIO_GPU = yes;
               EXT4_FS = yes;
+              USBIP_CORE = module ;
+              USBIP_VHCI_HCD = module;
+              USBIP_HOST = module;
+              USBIP_VUDC = module;
             };
           };
           makeModulesClosure = args: super.makeModulesClosure (args // {
